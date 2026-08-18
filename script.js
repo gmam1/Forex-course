e90bbabafd97d79b61930c0d4e59bf2626efd911
Ghulam Mohiuddin <ghulammohiuddinbhatti3@gmail.com>
2026-08-18 22:17:13 +0500

    Update script.js

--- script.js
+++ script.js
@@ -6,8 +6,8 @@
 */
 
 const courseProgress = {
-  classesHeld: 1,            // How many classes have been held so far (max final = 40)
-  assignmentsGiven: 0,       // How many assignments given so far (max final = 40)
+  classesHeld: 2,            // How many classes have been held so far (max final = 40)
+  assignmentsGiven: 0.5,       // How many assignments given so far (max final = 40)
   quizzesMaxMarks: 0,        // Current total possible quiz marks (e.g. 1 quiz = 5, 2 quizzes = 10...)
   finalExamHeld: false,      // true when final exam is conducted
   finalExamMax: 20           // Final exam total marks (usually 20)
