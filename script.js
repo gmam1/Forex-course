ed1d27881b23bd1d9f7cb7365f9ad9f503ec4492
Ghulam Mohiuddin <ghulammohiuddinbhatti3@gmail.com>
2026-08-31 22:41:14 +0500

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
 */
 
 const courseProgress = {
-  classesHeld: 10,            // How many classes have been held so far (max final = 40)
+  classesHeld: 11,            // How many classes have been held so far (max final = 40)
   assignmentsGiven: 7,       // How many assignments given so far (max final = 40)
   quizzesMaxMarks: 5,        // Current total possible quiz marks (e.g. 1 quiz = 5, 2 quizzes = 10...)
   finalExamHeld: false,      // true when final exam is conducted
@@ -45,7 +45,7 @@ const students = [
     id: 6,
     name: "Sehar",
     studentId: "260801005",
-    attendance: 7,
+    attendance: 8,
     assignments: 1.6,
     quizMarks: 3.25,
     finalExam: 0
